bd54e76af37f2e8f18baeadd708abc0d0f4c4ad7
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-07-15 21:26:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -802,8 +802,17 @@ eyad.channel.sendEmbed(Embed11).then(eyad => {eyad.delete(15000)})
 })
 
 
-
-
+client.on('message',async message => {
+const cd = require("countdown")
+      if (msg.content.toLowerCase() === `${prefix}timer`) {
+        let hi = await message.channel.send(`Wait.`)
+          setInterval(async function () {
+            let thedate = new Date().getTime();
+            let ramadan = new Date("May 6, 2019 00:00:00").getTime();
+            var timespan = cd(thedate, timer);
+            hi.edit(`Remaining sec until timer comes\n${timespan}`)
+          }, 1000);
+      }
 
 
       
