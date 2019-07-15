2214760f9f6eb7e1a050715c0e57feaa4a390538
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-07-15 21:42:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -886,5 +886,50 @@ hours = 12;
 
 
 
+client.on('message', message => {
+  if(message.content.startsWith(prefix + 'bc')) {
+  let args = message.content.split(" ").slice(1).join(" ");
+  if(!args) return message.channel.send(`**:rolling_eyes: please type the broadcast message**`)
+  let filter = m => m.author.id == message.author.id
+  let broadcastt = new Discord.RichEmbed()
+  .setColor('#36393e')
+  .addField(`**[1] broadcast for all members\n\n[2] broadcast for online members\n\n[0] to cansel**`,`** **`)
+  message.channel.send(broadcastt).then(msg => {
+  message.channel.awaitMessages(filter, {
+    max: 1,
+    time: 90000,
+    errors: ['time']
+  })
+  .then(collected => {
+    if(collected.first().content === '1') {
+      message.channel.bulkDelete(1)
+  message.channel.send(`**Broadcast begin send to \`${message.guild.members.size}\` members....**`);
+  msg.delete()
+     return message.guild.members.forEach(m => {
+  m.send(args.replace('[user]', m))
+      })
+  }
+  if(collected.first().content === '2') {
+    msg.delete()
+    message.channel.bulkDelete(1)
+    message.channel.send(`**Broadcast begin send to \`${message.guild.members.filter(m=>m.presence.status == 'online').size}\` members....**`);
+  message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {
+    m.send(args.replace('[user]', m))
+  })
+  message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {
+    m.send(args.replace('[user]', m))
+  })
+  return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {
+    m.send(args.replace('[user]', m))
+  })
+    }
+  if(collected.first().content === '0') {
+    message.channel.bulkDelete(1)
+    msg.delete()
+    return message.channel.send(`**Broadcast Has Been Canseled**`);
+  }})})}
+  });
+
+
       
 client.login(process.env.BOT_TOKEN);
